efafb62eb91c352239ce6cc72f9d63debf1eaed5
David Gardner <david_lng8@yahoo.com>
2025-06-01 20:25:42 -0400

    Configure docker container for both apps with hot reload

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from "@/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(parseInt(process.env.BACKEND_PORT || "3011"));
+  // For port binding towards
+  await app.listen(parseInt(process.env.BACKEND_PORT || "3011"), "0.0.0.0");
 }
 bootstrap();
