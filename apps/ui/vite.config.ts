8e2402a61a69f5c292386b0b450681e789bd63a9
David Gardner <david_lng8@yahoo.com>
2025-06-01 20:25:42 -0400

    Configure docker container for both apps with hot reload

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -1,13 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  // Load .env from the *monorepo root*
+  const env = loadEnv(mode, resolve(__dirname, "../../"), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.FRONTEND_PORT || "3110"),
+      proxy: {
+        "/api": `http://localhost:${env.BACKEND_PORT || "3011"}`,
+      },
     },
-  },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
